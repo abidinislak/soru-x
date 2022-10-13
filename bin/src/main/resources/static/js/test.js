32f5f8e40b1172c44eda66686afe357f9eda7a9b
abidinislak <abidinislak@gmail.com>
2022-10-13 04:47:46 +0300

    first

---
+++ bin/src/main/resources/static/js/test.js
@@ -0,0 +1,17 @@
+
+	$(document).ready(function () {
+
+	
+		    
+		    var table = $('#roles').DataTable({
+				"sAjaxSource": "/gettodos",
+				"sAjaxDataProp": "",
+				
+				"columns": [
+					{ "data": "tId"},
+					{ "data": "testTitleText"},
+								    
+				]
+			 });
+		});
+		
